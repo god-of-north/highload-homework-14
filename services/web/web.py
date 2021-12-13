ac1cccb99e293c0151c9e498c478ab6b44019931
Maksym Lesyk <mnl@industrialinnovationgroup.com>
2021-12-13 09:52:02 +0200

    nginx geoip2 test config added

---
+++ services/web/web.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    print(os.getenv("COUNTRY"), flush=True)
+    return f'''<!DOCTYPE html>
+<html>
+<head>
+<title>Projector 14 Homework</title>
+</head>
+
+<body>
+<h1>Hello World</h1>
+<h2>{os.getenv("COUNTRY")}</h2>
+</body>
+
+</html> 
+'''
+
+@app.route("/aaa/<xxx>")
+def index2(xxx):
+    return f'''<!DOCTYPE html>
+<html>
+<head>
+<title>Projector 14 Homework</title>
+</head>
+
+<body>
+<h1>Hello World</h1>
+<h2>var: {xxx}</h2>
+</body>
+
+</html> 
+'''
+
